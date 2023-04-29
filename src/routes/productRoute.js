a3a5463112ccb7e6bd96dc313193db26748de314
shhashank <shashanksherry@gmail.com>
2023-04-29 11:08:00 +0530

    mvcCommit

--- src/routes/productRoute.js
+++ src/routes/productRoute.js
@@ -1,6 +1,17 @@
 var express = require('express');
 var productController = require('../controllers/productController');
+const Product = require('../models/productModel');
+
 var router = express.Router();
-router.post('/user/product', productController.ram);
+router.post('/addProduct', productController.ram);
+router.post('/findProduct', async (req, res, next) => {
+  const name = req.body.name;
+
+  const product = await Product.find({ name: name }).sort({ date: -1 });
+  if (product) {
+    return res.status(200).send(product[0].type);
+  }
+  res.status(404).send('No orders found');
+});
 
 module.exports = router;
