52e64e8ec52d7844bacf8ad15170c31b1ad2a209
shhashank <shashanksherry@gmail.com>
2023-05-01 11:38:46 +0530

    cart-added

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const userRouter = require('./routers/user');
 const itemRouter = require('./routers/item');
+const cartRouter = require('./routers/cart');
 
 var cors = require('cors');
 require('dotenv').config();
@@ -13,6 +14,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(userRouter);
 app.use(itemRouter);
+app.use(cartRouter);
 
 const PORT = 5001;
 
